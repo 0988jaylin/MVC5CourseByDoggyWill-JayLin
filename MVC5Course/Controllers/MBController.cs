ec6400e5b9658e63f98ce7797b1ed413ba747b09
0988jaylin <0988jaylin@gmail.com>
2015-04-26 12:08:10 +0800

    練習 viewdata,tempdata,viewbag,session的使用方式與差異性

---
+++ MVC5Course/Controllers/MBController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC5Course.Models;
+
+namespace MVC5Course.Controllers
+{
+    public class MBController : BaseController
+    {
+        // GET: MB
+        public ActionResult Index()
+        {
+            Client client = db.Client.Find(100);
+            Product product = db.Product.Find(1);
+
+            ViewData.Model = client;
+            ViewBag.Product = product;
+
+            return View();
+        }
+
+        public ActionResult TempData1()
+        {
+            ViewData["hello1"] = "hello1 viewdata";
+            TempData["hello2"] = "hello2 tempdata";
+            Session["hello3"] = "hello3 session";
+            return RedirectToAction("TempData2");
+        }
+
+        public ActionResult TempData2()
+        {
+            ViewBag.hello1 = ViewData["hello1"];
+            ViewBag.hello2 = TempData["hello2"];
+            ViewBag.hello3 = Session["hello3"];
+            return View();
+        }
+    }
+}
