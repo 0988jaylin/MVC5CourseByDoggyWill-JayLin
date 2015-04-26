0367b9db46ed27e3d561b116423aa396eeb2c552
0988jaylin <0988jaylin@gmail.com>
2015-04-26 17:11:13 +0800

    練習custom error handling

---
+++ MVC5Course/Controllers/AFController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5Course.Controllers
+{
+    public class AFController : Controller
+    {
+        // GET: AF
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HandleError]
+        public ActionResult ShowError()
+        {
+            throw new Exception("Error Occured");
+            return View();
+        }
+    }
+}
